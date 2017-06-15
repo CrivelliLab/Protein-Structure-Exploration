7e24ad71f6c2d4fd507fa31b8aff8da7763eab56
Rafael Zamora <rz4@hood.edu>
2017-06-14 17:09:31 -0700

    Implemented MPI

--- src/EncodePDBs.py
+++ src/EncodePDBs.py
@@ -9,6 +9,9 @@ import numpy as np
 import itertools as it
 import time
 
+# MPI
+from mpi4py import MPI
+
 # Space Filling Curves
 from ZCurves import *
 
@@ -22,7 +25,7 @@ from scipy import misc
 from VisualizationTools import *
 
 # Global Variables
-folder = '../data/Ras-Gene-PDB-Files/'
+folder = '/home/rzamora/LBNL/Project/Protein-Structure-Prediction/data/Ras-Gene-PDB-Files/'
 dynamic_bounding = True
 sample_dim = 64
 range_ = [-50, 50]
@@ -38,91 +41,123 @@ axis_list = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
 theta_list = [(np.pi*i)/4  for i in range(8)]
 
 if __name__ == '__main__':
-    # Read PDBs
-    pdb_files = []
-    for line in sorted(os.listdir(folder)): pdb_files.append(line)
+    # MPI Init
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    cores = comm.Get_size()
     if debug:
-        print "Encoding PDBs in:", folder
-        print "Total PDB Entries:", len(pdb_files)
+        print("MPI Info...")
+        print "Rank:", rank
+        print "Number of Cores:", cores
 
-    # Run Statistics on PDBs
-    if stats: pdb_stats(pdb_folder)
+    # Root Node Data Init
+    if rank == 0:
+        # Read PDBs
+        pdb_files = []
+        for line in sorted(os.listdir(folder)): pdb_files.append(line)
+        if debug:
+            print "Encoding PDBs in:", folder
+            print "Total PDB Entries:", len(pdb_files)
 
-    # Generate Space Filling Curves
-    if debug: print("Generating 3D Curve...")
-    curve_3d = gen_zcurve_3D(pow(sample_dim, 3))
-    if debug: print("Generating 2D Curve...")
-    curve_2d = gen_zcurve_2D(pow(sample_dim, 3))
+        # Run Statistics on PDBs
+        if stats: pdb_stats(pdb_folder)
 
-    # Generate Rotations
-    if debug: print("Generating Rotations...")
-    base_rotations = []
-    for axis in axis_list:
-        base_rotations.append([])
-        for theta in theta_list:
-            rotation = rotation_matrix(axis, theta)
-            base_rotations[-1].append(rotation)
+        # Generate Space Filling Curves
+        if debug: print("Generating 3D Curve...")
+        curve_3d = gen_zcurve_3D(pow(sample_dim, 3))
+        if debug: print("Generating 2D Curve...")
+        curve_2d = gen_zcurve_2D(pow(sample_dim, 3))
 
-    base_indices = [[i for i in range(len(base_rotations[j]))] for j in range(len(base_rotations))]
-    indices = list(it.product(*base_indices))
+        # Generate Rotations
+        if debug: print("Generating Rotations...")
+        base_rotations = []
+        for axis in axis_list:
+            base_rotations.append([])
+            for theta in theta_list:
+                rotation = rotation_matrix(axis, theta)
+                base_rotations[-1].append(rotation)
+        base_indices = [[i for i in range(len(base_rotations[j]))] for j in range(len(base_rotations))]
+        indices = list(it.product(*base_indices))
+        rotations = []
+        for index in indices:
+            comb_rotation = []
+            for i in range(len(base_rotations)):
+                comb_rotation.append(base_rotations[i][index[i]])
+            rotations.append(comb_rotation)
+        rotations = np.array(rotations)
 
-    rotations = []
-    for index in indices:
-        comb_rotation = []
-        for i in range(len(base_rotations)):
-            comb_rotation.append(base_rotations[i][index[i]])
-        rotations.append(comb_rotation)
-    rotations = np.array(rotations)
+        # Generate Entries
+        entries = []
+        for i in range(len(pdb_files)):
+            for j in range(len(rotations)):
+                entries.append([pdb_files[i], rotations[j], j])
+        entries = np.array(entries, dtype=object)
 
-    # Process PDB Entries
-    for pdb_file in pdb_files:
-        if debug: print('Processing ' + pdb_file + '...')
+    else:
+        curve_3d = None
+        curve_2d = None
+        entries = None
 
-        # Process Rotations
-        for j in range(len(rotations)):
-            if debug: start = time.time()
-            if debug: print('Processing Rotation ' + str(j) + '...')
-            rot = rotations[j]
-            pdb_data, dia = get_pdb_data(folder + pdb_file, channels=sel_channels, rot=rot, debug=debug)
-            dia += 2
+    # MPI Broadcast Data
+    if debug: print("Broadcasting Data To Nodes...")
+    curve_3d = comm.bcast(curve_3d, root=0)
+    curve_2d = comm.bcast(curve_2d, root=0)
+    entries = comm.bcast(entries, root=0)
 
-            # Process Channels
-            encoded_pdb_2d = []
-            pdb_3d_model = []
-            for i in range(len(pdb_data)):
-                if debug: print('Processing Channel ' + str(i) + '...')
-                pdb_data_res = pdb_data[i]
-                if pdb_data_res is None: continue
+    # MPI Cut Entries Per Node
+    entries = np.array_split(entries, cores)[rank]
+    if debug:
+        print "MPI Core", rank
+        print "Processing", len(entries), "Entries"
 
-                # Generate PDB Channel 3D Voxel Model
-                if dynamic_bounding:
-                    bounds = [pow(-1,i+1)*dia for i in range(6)]
-                    pdb_3d_res = gen_3d_pdb(pdb_data_res, bounds, sample_dim, debug=debug)
-                else:
-                    bounds = range_ + range_ + range_
-                    pdb_3d_res = gen_3d_pdb(pdb_data_res, bounds, sample_dim, debug=debug)
-                pdb_3d_model.append(pdb_3d_res)
+    # Process Rotations
+    for i in range(len(entries)):
+        if debug: start = time.time()
+        if debug: print('Processing Entry ' + str(i) + '...')
+        pdb_file = entries[i][0]
+        rot = entries[i][1]
+        rot_id = entries[i][2]
+        pdb_data, dia = get_pdb_data(folder + pdb_file, channels=sel_channels, rot=rot, debug=debug)
+        dia += 2
 
-                # Encode 3D Model with Space Filling Curve
-                encoded_res_2d = encode_3d_pdb(pdb_3d_res, curve_3d, curve_2d, debug=debug)
-                encoded_pdb_2d.append(encoded_res_2d)
+        # Process Channels
+        encoded_pdb_2d = []
+        pdb_3d_model = []
+        for i in range(len(pdb_data)):
+            if debug: print('Processing Channel ' + str(i) + '...')
+            pdb_data_res = pdb_data[i]
+            if pdb_data_res is None: continue
 
-            # Transpose Array
-            encoded_pdb_2d = np.array(encoded_pdb_2d)
-            encoded_pdb_2d = np.transpose(encoded_pdb_2d, (2,1,0))
+            # Generate PDB Channel 3D Voxel Model
+            if dynamic_bounding:
+                bounds = [pow(-1,i+1)*dia for i in range(6)]
+                pdb_3d_res = gen_3d_pdb(pdb_data_res, bounds, sample_dim, debug=debug)
+            else:
+                bounds = range_ + range_ + range_
+                pdb_3d_res = gen_3d_pdb(pdb_data_res, bounds, sample_dim, debug=debug)
+            pdb_3d_model.append(pdb_3d_res)
 
-            # Save Encoded PDB to Numpy Array File.
-            if debug: print("Saving Encoded PDB...")
-            file_path = '../data/Processed-'+ folder[8:] + pdb_file.split('.')[0] + '_'+ str(j) +'.png'
-            #np.savez_compressed(file_path, a=encoded_pdb_2d, b=rot)
-            misc.imsave(file_path, encoded_pdb_2d)
+            # Encode 3D Model with Space Filling Curve
+            encoded_res_2d = encode_3d_pdb(pdb_3d_res, curve_3d, curve_2d, debug=debug)
+            encoded_pdb_2d.append(encoded_res_2d)
 
-            if debug: print "Processed in: ", time.time() - start, ' sec'
+        # Transpose Array
+        encoded_pdb_2d = np.array(encoded_pdb_2d)
+        encoded_pdb_2d = np.transpose(encoded_pdb_2d, (2,1,0))
 
-            # Visualize PDB Data
-            if visualize:
-                print('Visualizing All Channels...')
-                display_3d_array(pdb_3d_model)
-                display_2d_array(encoded_pdb_2d)
+        # Save Encoded PDB to Numpy Array File.
+        if debug: print("Saving Encoded PDB...")
+        foldername = folder.split('/')[-2]
+        file_path = folder[:-len(foldername)-1]+'Processed-'+ foldername +'/'+ pdb_file.split('.')[0] + '_'+ str(rot_id) +'.png'
+        #np.savez_compressed(file_path, a=encoded_pdb_2d, b=rot)
+        misc.imsave(file_path, encoded_pdb_2d)
 
-            if debug: exit()
+        if debug: print "Processed in: ", time.time() - start, ' sec'
+
+        # Visualize PDB Data
+        if visualize:
+            print('Visualizing All Channels...')
+            display_3d_array(pdb_3d_model)
+            display_2d_array(encoded_pdb_2d)
+
+        if debug: exit()
