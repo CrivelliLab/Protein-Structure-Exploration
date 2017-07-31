2a0b385ffb708c0f42972495e5c56c12dd3926d7
Tom Corcoran <tomcorcoran@protonmail.com>
2017-07-31 10:20:23 -0700

    Updated .gitignore for new model run outputs (history items via pickle). Also updating psiblast dataset readme after manipulation for 7 card DGX-1 run.

---
+++ data/processed/datasets/psiblast/README.txt
@@ -0,0 +1,18 @@
+The HH-512-MS-FULL dataset has been modified in order to fit on 7 cards.
+Originally it was of size:
+
+train/ (total both classes): 142,500
+    subtracted 15 files from positive class in order to make the total 142,485
+    for even divsibiltiy by 21 (7 cards, 3 each per batch)
+
+test/ (total both classes): 35,600
+    subtracted 5 files from the positive class in order to make the total
+    35,595 for even divisibility by 21 (same reasoning as above). 
+
+These files were not deleted but were moved into their respective train/pos and
+test/pos folders under ./SUBTRACTED_ELEMENTS_HH-512-MS-FULL so that the
+original dataset can be rebuild with simple moves if needed. 
+
+Move operations were accomplished via:
+
+shuf -n 10 -e * | xargs -i mv {} path-to-new-folder
--- src/models_and_outputs/kras_hras/small_tester_setup/network_definition/TrainModel.py
+++ src/models_and_outputs/kras_hras/small_tester_setup/network_definition/TrainModel.py
@@ -1,15 +1,15 @@
 '''
 TrainModel.py
-Updated: 07/12/17
-[NOT PASSING] - Unimplemented Functionality
-                |- Saving Training History to File
+Updated: 07/30/17
+
+[PASSING]
 
 README:
 
 The following script is used to train networks on 2D PDB encodings.
 
 Global variables used during training are defined under #- Global Variables.
-'data_folders' defines the folder containing segemented encoded PDBs. Folders must
+'data_folder_name' defines the folder containing segemented encoded PDBs. Folders must
 be under data/processed/datasets/ .
 
 'network' defines the keras neural network which will be trained.
@@ -18,11 +18,25 @@ Note: Network must be imported.
 'image-size' defines the shape of the images used for training.
 'seed' defines the value used for random number generation.
 
+This script saves a history dictionary to a file called 'train_history_dict' as
+well as loss information to a csv named 'loss_history.csv'.
+
+In order to make use of this script for training on new data you must specify
+the correct data folder name, the correct number of gpus to be used in
+training, the correct image dimensions, the correct batch sizes and
+corresponding training and validation steps, and the number of epochs you wish
+to train for. 
+
+TODO:
+    Turn this into a master script that allows for more automated setting of
+    run parameters. Right now this is very manual. 
+
 '''
 import os, sys
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
+import cPickle as pickle
 
 # Networks
 from CIFAR_512 import CIFAR_512
@@ -58,7 +72,7 @@ if __name__ == '__main__':
 
     # Fit Training Data
     if debug: print "Training Network..."
-    history = network.model.fit_generator(train_flow, epochs=500,
+    history = network.model.fit_generator(train_flow, epochs=1,
             steps_per_epoch=800,
                 validation_data=test_flow, callbacks=[save,],
                 validation_steps=200)
@@ -67,3 +81,7 @@ if __name__ == '__main__':
     loss_history = history.history["loss"]
     loss_history = np.array(loss_history)
     np.savetxt(output_folder+"loss_history.csv", loss_history, delimiter=',')
+
+    print('type(history):', type(history))
+    with open(output_folder + 'train_history_dict', 'wb') as pf:
+        pickle.dump(history.history, pf)
