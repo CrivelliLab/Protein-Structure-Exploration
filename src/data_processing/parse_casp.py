4850faa89d4aef646df1cbeee4209bceed6b4a9b
rz4 <rz4@hood.edu>
2018-02-08 11:12:58 -0800

    Updated casp pareser

--- src/data_processing/parse_casp.py
+++ src/data_processing/parse_casp.py
@@ -8,10 +8,10 @@ from shutil import copyfile
 import numpy as np
 
 # Data folder path
-data_folder = '../../data/T0887/'
+data_folder = '../../data/T0882/'
 
 # Parameters
-bins = [37.5,100.0] # Bin cut-offs in ascending order
+bins = [0.375, 1.0] # Bin cut-offs in ascending order
 
 ###############################################################################
 
@@ -25,15 +25,16 @@ if __name__ == '__main__':
     # Gather ids and GDTTS scores from results .txt
     ids = []
     scores = []
-    with open(data_folder+data_folder.split('/')[-2]+'.txt', 'r') as f:
+    with open(data_folder+data_folder.split('/')[-2]+'.csv', 'r') as f:
         lines = f.readlines()
         for i in range(len(lines)):
-            x = lines[i].split()
-            if i > 1 and len(x) == 49:
-                id_ = x[1]
-                score = float(x[3])
+            x = lines[i].split(',')
+            if i > 1 and len(x) == 4:
+                id_ = x[0]
+                score = float(x[1])
                 ids.append(id_)
                 scores.append(score)
+    print(len(ids))
 
     # Bin ids according to score as defined in bins parameter
     binned_ids = []
@@ -49,10 +50,12 @@ if __name__ == '__main__':
                 ids_neg.append(ids[j])
                 scores_neg.append(scores[j])
 
+        print(len(ids_pos))
         binned_ids.append(ids_pos)
         ids = ids_neg
         scores = scores_neg
 
+    '''
     # Copy PDBs into folders
     for i in range(len(bins)):
         bin_ = bins[i]
@@ -60,4 +63,5 @@ if __name__ == '__main__':
         for j in range(len(binned_ids[i])):
             id_ = binned_ids[i][j]
             if not os.path.exists(data_folder+'<'+str(bin_)+'/'+id_): os.mkdir(data_folder+'<'+str(bin_)+'/'+id_)
-            copyfile(data_folder+'/pdbs/'+id_, data_folder+'<'+str(bin_)+'/'+id_+'/'+id_+'.pdb')
+            copyfile(data_folder+'/pdbs/'+id_+'.pdb', data_folder+'<'+str(bin_)+'/'+id_+'/'+id_+'.pdb')
+    '''
